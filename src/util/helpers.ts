80e22bbdba6089f455cb4f411244717d6f1f9c68
Kenny Durkin <hi@kennydurk.in>
2023-09-11 10:56:16 -0700

    Begin TS migration

--- src/util/helpers.ts
+++ src/util/helpers.ts
@@ -3,18 +3,24 @@ export function timeout(ms) {
 };
 
 const pluginPosition = "initial-plugin-position";
-const getPluginEl = () => {
+const getPluginEl = () : HTMLElement|null => {
     const pluginSelector = ".mapboxgl-ctrl-directions";
     return document.querySelector(pluginSelector);
 }
 
 export function cleanupPluginPosition() {
     const pluginEl = getPluginEl();
+    if (!pluginEl) {
+        return;
+    }
     pluginEl.classList.remove(pluginPosition);
 
     const bottomLeftEl = document.querySelector(".mapboxgl-ctrl-bottom-left");
     if (pluginEl.parentNode === bottomLeftEl) {
         const topLeftEl = document.querySelector(".mapboxgl-ctrl-top-left");
+        if (!topLeftEl) {
+            return;
+        }
         topLeftEl.appendChild(pluginEl);
         pluginEl.style.bottom = '';
     }
@@ -27,8 +33,14 @@ export function initializePluginPosition() {
     // Address bar height needs to be calculated and figured into the bottom offset for this element
     // Necessary on mobile devices because the sidebar el, for some reason, uses the viewport bottom
     // when set to bottom: 0 rather than the true bottom zero point which is offset by address bars
-    const addressBarHeight = document.querySelector('.map-container').offsetHeight - window.innerHeight;
-    const sidebarElHeight = document.querySelector('.journey-sidebar').offsetHeight;
+    const addressBar: HTMLElement|null = document.querySelector('.map-container');
+    const sidebarEl: HTMLElement|null = document.querySelector('.journey-sidebar');
+    if (!addressBar || !sidebarEl) {
+        return;
+    }
+
+    const addressBarHeight = addressBar.offsetHeight - window.innerHeight;
+    const sidebarElHeight = sidebarEl.offsetHeight;
     const bottomOffset = addressBarHeight + sidebarElHeight;
 
     pluginEl.style.bottom = `${bottomOffset}px`;
@@ -37,8 +49,11 @@ export function initializePluginPosition() {
 
 // Cosmetic, though it'd be nice if there were a cleaner way to do this
 export function toggleDestinationUI(shouldShow) {
-    const destination = document.querySelector('.mapbox-directions-destination')
+    const destination: HTMLElement|null = document.querySelector('.mapbox-directions-destination')
+    const reverseSymbol: HTMLElement|null = document.querySelector('.directions-icon-reverse')
+    if (!destination || !reverseSymbol) {
+        return;
+    }
     destination.style.display = shouldShow ? 'block' : 'none';
-    const reverseSymbol = document.querySelector('.directions-icon-reverse')
     reverseSymbol.style.display = shouldShow ? 'block' : 'none';
   }
